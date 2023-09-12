043d0583e069982253e078b59d851baa404827ba
EsToHi <31039681+EsToHi@users.noreply.github.com>
2023-09-12 15:42:08 +0100

    Convert2.sh

--- L/Convert.sh
+++ L/Convert.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-# Define the input text file and output directory
-input_file="molecule_list.txt"
-output_dir="output_pdbqt_files"
+# Read the ALL.txt file line by line
+while IFS=$'\t' read -r name smiles; do
+    # Remove any leading/trailing whitespaces
+    name=$(echo "$name" | awk '{$1=$1};1')
+    smiles=$(echo "$smiles" | awk '{$1=$1};1')
 
-# Create the output directory if it doesn't exist
-mkdir -p "$output_dir"
+    # Generate the PDBQT file using obabel
+    obabel -:"$smiles" -O "${name}.pdbqt" --gen3d
 
-# Loop through each line in the input file
-while IFS=$'\t' read -r name smile; do
-  # Remove spaces from the name to create a valid output filename
-  output_filename="${name// /_}.pdbqt"
-  
-  # Use Open Babel to convert the SMILES string to PDBQT format
-  obabel -:"$smile" -O "$output_dir/$output_filename" --gen3d
-  
-  echo "Converted $name to $output_filename"
-done < "$input_file"
-
-echo "Conversion complete!"
+    echo "Converted $name to PDBQT"
+done < ALL.txt
